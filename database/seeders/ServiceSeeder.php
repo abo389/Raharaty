f4ca5b9fe90056deb6a814ebb3ec3c539e16f6e5
abdo389 <001abo001@gmail.com>
2024-12-31 13:02:23 +0200

    fix images

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -17,17 +17,17 @@ class ServiceSeeder extends Seeder
             [
                 "title" => 'دليل سياحي',
                 "description" => 'نقدم لك خدمات الإرشاد السياحي لبناء خطط سفر مميزة',
-                "icon_link" => 'https://s3-alpha-sig.figma.com/img/91da/98ad/df87fbf9c1c51598faa214456f6d6234?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ecBQ6Hd-6ln5QURGTls7fw6OtXVMlkPab2rcd~RtVNnpBgWl~Xdspua6I0Z86hxqwU-fl8VGKgM~IMkAyFBR9yBr7RNyRL0kRsgR1ZIy8GPFcSfgJ4XIZl99e~IGYjvv1nBlbUbNSmmE2jXstkqlq7UEve-bVW5bItIN~3jfvgFkq~lw~f08OgP4tp4gVattXGHr2L1wEsPg3Xw5nBw7Ql0AHVWFAgqKy7B4EXkv7qg0TAaiHN4J0QM3j2F8cCc~hWCrctD9OEPyhTk7Gr5QHnd4bRPqIM5F42CrT6lKt2Gs8s9GOLzwuOHSEiFSLyu6P-d6f6ljCN3lL0IEJOM8Sw__'
+                "icon_link" => 'icon-1.png'
             ],
             [
                 "title" => 'حجوزات فندقية',
                 "description" => 'لا تقلق حول مكان إقامتك فنحن نهتم بكامل الحجوزات الخاصة بك',
-                "icon_link" => 'https://s3-alpha-sig.figma.com/img/0568/1dda/8e63aa35cd2b6a9a4ceb3f13021dbd1c?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=VSEO5tP~n11W0q8A~SyuIx989FMJ8SUSAo06jiavSmP2pXssTcI-7m3Mr2e9JE-o5bWPzdCDgiwTWYetgr~Z~7LY-IiOSALfSPSRuFYy9AGpfdbrjLAi-O7HJi0~6T8Vj7Cfw9GvZTmFMd~NjBPEpBrpRij6DnozVCcoRYJN3s2Z8LfsrD~3mOPx-aYd07tUoF-lXVhFOUw9zH4iiHoYQrdGEoYIW4b8SBZPKVRLfX7l0Q3H6OGxaN8ArQMALrVmikd2FMBdw5ybXbA9Sy6xtgBkNOZHE0nwpQLq6V-GJBDzKELCVdJ3iEGIOIk4V~bqznmK1E~81eYTFFNzTxkfEQ__'
+                "icon_link" => 'icon-2.png'
             ],
             [
                 "title" => 'رحلات ميدانية',
                 "description" => 'لن تكون إجازتك مملة معنا فهنا الكثير من الخطط والرحلات المختلفة',
-                "icon_link" => 'https://s3-alpha-sig.figma.com/img/a982/a9d8/d8a506554803392278fdc3e112afff46?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=F0BOeL7ldKA40xCopnR3FfaaEHFnXA18xMc3nO6yIB8L1TJjRhWkJq2gfKzjH-uO5xO-s7OkBgER2r2sVmLFgPeYMXmBZqboA3RhD4B11zebviNP6gxpLJ5j7xo2spuUUMR3kGxMw6mSSxDThYVy1nOs5me6sKGD3Zxw8HmrHrRfB9jiOHdK66yN6nOQFqANWus38xxHeeEfUG8ikaZLLx5PzkMY6O4TRKOEIglxRuy17dMEAUAuaK4rOisqMySUt~egt-FlgCG3mHsn4W895scPb22-WzrfqStj8TuHlMAeoMAzhbMzUjMLk~3y044Tcz1VDSJGwiAjxSQbwmIpZg__'
+                "icon_link" => 'icon-3.png'
             ]
         ];
 
--- database/seeders/TripSeeder.php
+++ database/seeders/TripSeeder.php
@@ -16,25 +16,25 @@ class TripSeeder extends Seeder
 
         $trips = [
             [
-                "image_link" => 'https://s3-alpha-sig.figma.com/img/47f1/c27c/f937bd2041f53c6be27a0ab05d6e5fb0?Expires=1736726400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=IWV4~cUlY832wT1f1ZzMyBmxGaPypdxHXKD1EArZ6aVonMz56UNA4MmNuVcyCzJkdFdfBqFWi19WJxUTL3rWVCM1NzUlJdhqI5lh1fJzpaHhV68Dr6VXvH05kQbprnVBEe9OZVWQ3PThjNV-qYVRqDdTZGznJeqPA3U2EbrqUJ8BVUquMB5Wris7k6veXSCL1-73BvPgOOwj35yO-AXtiQutptbcHBCVn9xxQULv59RY9xpmrkt8JH-E-0GY4jQ8DenOzNJkz41pIO7n~DQp3Ixgo5fT5h4o3Hg8Ya9uESbGh6tChOGyPSVBUhenWodXDZEMt6exRrfLnEH-dAjoZg__',
+                "image_link" => 'trip-1.png',
                 "city" => 'اسطنبول',
                 "day_num" => '5',
                 "cost" => '1500'
             ],
             [
-                "image_link" => 'https://s3-alpha-sig.figma.com/img/55bd/f8a5/4105c492b21062d6d9b6a423915b2087?Expires=1736726400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jY2CL-u6OPihPcvCBg2gVhoXh85h5o-NDEkgqguZQNozUC12vYRNRS1hEJfMBlpFu~G6-XVF83~SI3FgQEYZamgSjRIB7CJ~Fo1e0yCtoTWo5spj~s~oa2i7W3kXWUii1E~m8lX6EVzvONMmYWMSao4-d-dqDsHbLFcLRB~RFE~ongXXpEsN7p802Jx0QxFpPQ8hrwP6BC4iNOU3jXHs~xl9YoaKmosz-8361VtClabDtUSgq~AOIw5om1CZp8Fh1Kku7jUQXuqyxBRBlFJLo11B2lvQ02~7ejLB2L3hXVQsFMSfgXMCqs~sWifq0vNtRnK7PeT8nSEkqln~cAjmvQ__',
+                "image_link" => 'trip-2.png',
                 "city" => 'دبي',
                 "day_num" => '3',
                 "cost" => '2500'
             ],
             [
-                "image_link" => 'https://s3-alpha-sig.figma.com/img/a283/6142/931b2a24ce57a874391912e9b764dd25?Expires=1736726400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dB4ZrIw0P3OWTXVKYmdXMbulvN~hbWM70yhcFlmfb6NPS3K~WG6RrQfPHuAOk-U8Ku7zqmVkSczu2dIrfcPxXIP53zwG7MPh4k125MGRLuYFUE4jzSLj6wx-gN~f7vvct2ijhWsqeORf3oJsY-4WsDyP~Ol3mmQh442hsLjyEiFFOmLzfeOMqT9NeswkMaaOfSBLpf5UQp-YrrC0sMD22QT-~juUdjrWMyabqqim0IN3B0r5d9oA5HnPwbMGzJM3oTovQpBqh-MEqFLCm8Y-PhmGdKiekfvtsWyrlBn4jrxqSe1N2e4LldO1cSomlBqb9UxwIR~r4ioUMrzynFVmYQ__',
+                "image_link" => 'trip-3.png',
                 "city" => 'القاهرة',
                 "day_num" => '3',
                 "cost" => '1250'
             ],
             [
-                "image_link" => 'https://s3-alpha-sig.figma.com/img/9f21/d0a9/e2bcf92d6fa9f81b6be74d93d652acf8?Expires=1736726400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=BnK0PRoxtCiGBhjG6ADhoefnrsw0kbMcEqLD6vR92lAAtICNihtHy~oPN5UhumAjNDzs74YcFnpDr1exX9~ga3uvIRiu0bkyBZow9300bHmNuDrkXDfKO-2Clao5Rcmpvt5xS4c-ZIv00U6YtvRfcxAFDsxDjhxNrBvCYyH3RCWjGRsiIjntY58u1xIAg4zDcau10NGnbRdYgCektVzEkyM2OILor7mO6vH9614Is~Te-bJQmzcBsPWUCYi75Z5dq3LPClNq8RQZCnhJQ-RBIotIUCCwr~JAeGNN1MQgq3yaWpmZsV-FVBEm1jAuKXlvT~5RxYvkVAYZlweVLLPhgA__',
+                "image_link" => 'trip-4.png',
                 "city" => 'الرياض',
                 "day_num" => '2',
                 "cost" => '1500'
